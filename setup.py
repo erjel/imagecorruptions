e1682a7cda24d20dc94b4e1756516ee75abdce11
Eric Jelli <eric.jelli@mpinb.mpg.de>
2025-03-14 10:01:54 +0100

    bump version number

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="imagecorruptions-imaug",
-    version="1.1.3rc0",
+    version="1.1.3rc1",
     author="imaug",
     author_email="ej_foss@mailbox.org",
     description="This package provides a set of image corruptions.",
